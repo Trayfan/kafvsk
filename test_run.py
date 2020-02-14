de94bed7e2c6a7a0c64f332faef78f6c2887d116
Соколов Алексей Анатольевич <AASokolov@VSK.RU>
2020-02-14 13:07:42 +0300

    add new test

--- test_run.py
+++ test_run.py
@@ -1,11 +1,18 @@
+import customLogger as cl
 import tests.contracts.REG_753.organization.Code_0.test
 import tests.kbm.REG_583_REG_820.get_kbm.person_1.test
 import tests.kbm.REG_583_REG_820.get_kbm.person_2.test
 import tests.kbm.REG_583_REG_820.get_kbm.organization_1.test
 import tests.contracts.REG_578.BSO.Code_0.test
+import tests.contracts.REG_578.BSO.Method.test
+
+
+logger = cl.custom_logger()
+
 
 # tests.contracts.REG_753.organization.Code_0.test.Test_sub().test()
-tests.contracts.REG_578.BSO.Code_0.test.Test_sub().test()
+tests.contracts.REG_578.BSO.Method.test.Test_sub(logger).test()
+# tests.contracts.REG_578.BSO.Code_0.test.Test_sub(logger).test()
 # tests.kbm.REG_583_REG_820.get_kbm.person_1.test.test()
 # tests.kbm.REG_583_REG_820.get_kbm.person_2.test.test()
 # tests.kbm.REG_583_REG_820.get_kbm.organization_1.test.test()
--- tests/contracts/REG_578/BSO/Method/test.py
+++ tests/contracts/REG_578/BSO/Method/test.py
@@ -2,14 +2,17 @@ from MainTest import MainTest
 
 
 
+
 class Test_sub(MainTest):
     def __init__(self, cl):
         self.logger = cl
-        self.test_name = r"Проверить заполнение ответа при успешной загрузке заключенного договора ОСАГО (Type = BSO) (Errors.Code=0)"
+        self.test_name = r"Проверить заполнение ответа при указании невалидного значения атрибута Method (Errors.Code=1)"
         self.logger.info(f"# Start test {self.test_name}")
-        self.PATH = r".\tests\contracts\REG_578\BSO\Code_0"
+
+        
+        self.PATH = r".\tests\contracts\REG_578\BSO\Method"
         self.REQUEST_FILE = r"NewContractRequest.json"
         self.RESPONSE_FILE = r"NewContractResponse.json"
         self.request_topic = r"ContractsRequest"
-        self.response_topic = r"ContractsResponse"
+        self.response_topic = r"ContractsRequest"
         MainTest.__init__(self)
